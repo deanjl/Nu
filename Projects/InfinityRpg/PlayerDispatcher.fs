6aa9cb856c006cf7d9b712442959064bbfce8626
Dean J Lee <djl6@hotmail.com>
2020-05-11 08:43:15 +1000

    Put InfinintyRpg back in

---
+++ Projects/InfinityRpg/PlayerDispatcher.fs
@@ -0,0 +1,18 @@
+﻿namespace InfinityRpg
+open System
+open Prime
+open Nu
+open Nu.Declarative
+open InfinityRpg
+
+[<AutoOpen>]
+module PlayerDispatcherModule =
+
+    type PlayerDispatcher () =
+        inherit CharacterDispatcher ()
+
+        static member Facets =
+            [typeof<CharacterCameraFacet>]
+
+        static member Properties =
+            [define Entity.CharacterState { CharacterState.empty with HitPoints = 30; ControlType = PlayerControlled }]
