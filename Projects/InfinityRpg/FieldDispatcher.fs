b0db5fbe2545272f217c5e5c89fab299fcbf23f1
Dean J Lee <djl6@hotmail.com>
2020-05-22 16:04:59 +1000

    brought tile-culling code up to standard

--- Projects/InfinityRpg/FieldDispatcher.fs
+++ Projects/InfinityRpg/FieldDispatcher.fs
@@ -32,6 +32,21 @@ module FieldDispatcherModule =
                      tileOffset.Y + Constants.Layout.TileSize.Y)
             Some tileInset
 
+        static let viewBoundsToMapUnits (viewBounds : Vector4) =
+            let right = int viewBounds.Z
+            let top = int viewBounds.W
+            Vector4i
+                (itom (int viewBounds.X),
+                 itom (int viewBounds.Y),
+                 (if isSnapped right then (itom right) - 1 else itom right),
+                 (if isSnapped top then (itom top) - 1 else itom top))
+
+        static let tilePositionInView (tilePositionM : Vector2i) (mInViewBounds : Vector4i) =
+            tilePositionM.X >= mInViewBounds.X &&
+            tilePositionM.Y >= mInViewBounds.Y &&
+            tilePositionM.X <= mInViewBounds.Z &&
+            tilePositionM.Y <= mInViewBounds.W
+        
         static member Properties =
             [define Entity.Omnipresent true
              define Entity.FieldMapNp DefaultFieldMap]
@@ -50,30 +65,27 @@ module FieldDispatcherModule =
             if World.isBoundsInView viewType bounds world then
                 let fieldMap = field.GetFieldMapNp world
                 let image = fieldMap.FieldTileSheet
-                let mInViewBounds = World.getViewBounds viewType world |> MInViewBounds.make
-                let tiles =
-                    fieldMap.FieldTiles
-                    |> Map.filter (fun k v -> k.X >= mInViewBounds.Left)
-                    |> Map.filter (fun k v -> k.Y >= mInViewBounds.Bottom)
-                    |> Map.filter (fun k v -> k.X <= mInViewBounds.Right)
-                    |> Map.filter (fun k v -> k.Y <= mInViewBounds.Top)
+                let mInViewBounds = World.getViewBounds viewType world |> viewBoundsToMapUnits
+                let tiles = fieldMap.FieldTiles
                 let sprites =
                     Map.foldBack
                         (fun tilePositionM tile sprites ->
-                            let tilePosition = vmtovf tilePositionM // NOTE: field position assumed at origin
-                            let tileInsetOpt = getTileInsetOpt tile.TileSheetPositionM
-                            let sprite =
-                                { Position = tilePosition
-                                  Size = Constants.Layout.TileSize
-                                  Rotation = 0.0f // NOTE: rotation assumed zero
-                                  Offset = Vector2.Zero
-                                  ViewType = Relative // NOTE: ViewType assumed relative
-                                  InsetOpt = tileInsetOpt
-                                  Image = image
-                                  Color = Vector4.One
-                                  Glow = Vector4.Zero
-                                  Flip = FlipNone }
-                            sprite :: sprites)
+                            if tilePositionInView tilePositionM mInViewBounds then
+                                let tilePosition = vmtovf tilePositionM // NOTE: field position assumed at origin
+                                let tileInsetOpt = getTileInsetOpt tile.TileSheetPositionM
+                                let sprite =
+                                    { Position = tilePosition
+                                      Size = Constants.Layout.TileSize
+                                      Rotation = 0.0f // NOTE: rotation assumed zero
+                                      Offset = Vector2.Zero
+                                      ViewType = Relative // NOTE: ViewType assumed relative
+                                      InsetOpt = tileInsetOpt
+                                      Image = image
+                                      Color = Vector4.One
+                                      Glow = Vector4.Zero
+                                      Flip = FlipNone }
+                                sprite :: sprites
+                            else sprites)
                         tiles [] |>
                     Array.ofList
 
