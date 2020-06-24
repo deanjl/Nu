354cbf36be1351c4602d8b48e549965f23fc3885
Dean J Lee <djl6@hotmail.com>
2020-06-24 14:17:00 +1000

    began elmifying character

--- Projects/InfinityRpg/CharacterDispatcher.fs
+++ Projects/InfinityRpg/CharacterDispatcher.fs
@@ -8,8 +8,16 @@ open InfinityRpg
 [<AutoOpen>]
 module CharacterDispatcherModule =
 
+    type [<StructuralEquality; NoComparison>] CharacterModel =
+        { CharacterActivityState : CharacterActivityState
+          CharacterState : CharacterState }
+
+        static member initial =
+            { CharacterActivityState = NoActivity
+              CharacterState = CharacterState.empty }
+    
     type CharacterDispatcher () =
-        inherit EntityDispatcher ()
+        inherit EntityDispatcher<CharacterModel, unit, unit> (CharacterModel.initial)
 
         static member Facets =
             [typeof<CharacterStateFacet>
