4492e0bf321448d38058a2d91f877c62d75f4202
Dean J Lee <djl6@hotmail.com>
2020-10-10 10:46:06 +1100

    began work on pickup items

--- Projects/InfinityRpg/InfinityPickup.fs
+++ Projects/InfinityRpg/InfinityPickup.fs
@@ -4,3 +4,23 @@ open Prime
 open Nu
 open Nu.Declarative
 open InfinityRpg
+
+[<AutoOpen>]
+module PickupDispatcherModule =
+
+    type PickupType =
+        | Health
+    
+    type [<StructuralEquality; NoComparison>] PickupModel =
+        { PickupType : PickupType }
+
+        static member health =
+            { PickupType = Health }
+
+    type Entity with
+        member this.GetPickupModel = this.GetModel<PickupModel>
+        member this.SetPickupModel = this.SetModel<PickupModel>
+        member this.PickupModel = this.Model<PickupModel> ()
+
+    type PickupDispatcher () =
+        inherit EntityDispatcher<PickupModel, unit, unit> (PickupModel.health)
